17addc6fe85918eb4d0accf32adcd1113f4a643d
BrindhaP2024 <brindha.palani@aspiresys.com>
2025-02-17 16:58:31 +0530

    Admin dashboard,add ,delete, update products

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,21 +1,22 @@
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/products.service';
 import { product } from '../../interfaces/data-type';
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports:[CommonModule,RouterLink,FontAwesomeModule],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
   productList: product[] | undefined;
   productMessage: string | undefined;
-  icon = faTrash;
+  iconDelete = faTrash;
   iconEdit = faEdit;
 
   constructor(private productService: ProductService) {}
@@ -25,14 +26,14 @@ export class DashboardComponent implements OnInit {
   }
 
   listProducts(): void {
-    this.productService.getProductList().subscribe((result) => {
+    this.productService.productList().subscribe((result) => {
       this.productList = result;
     });
   }
 
   deleteProduct(id: number): void {
     this.productService.deleteProduct(id).subscribe(() => {
-      this.productMessage = 'Product is deleted';
+      this.productMessage = 'Product has been deleted successfully!';
       this.listProducts();
       setTimeout(() => {
         this.productMessage = undefined;
